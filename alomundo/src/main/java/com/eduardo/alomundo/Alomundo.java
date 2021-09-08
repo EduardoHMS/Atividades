8f478214df9b7e8d30d74db35052b59d8715566e
Eduardo Henrique <eduardohenriquemoreirasilva@gmail.com>
2021-09-08 20:16:42 -0300

    WireFrames

--- alomundo/src/main/java/com/eduardo/alomundo/Alomundo.java
+++ alomundo/src/main/java/com/eduardo/alomundo/Alomundo.java
@@ -8,16 +8,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class Alomundo {
 
-    @RequestMapping("alo")
-    public String alo(Model modelo){
-        Random r = new Random();
-        modelo.addAttribute("nro1", r.nextInt());
-        modelo.addAttribute("nro2", r.nextInt());
-        modelo.addAttribute("nro3", r.nextInt());
-        modelo.addAttribute("nro4", r.nextInt());
-        modelo.addAttribute("nro5", r.nextInt());
-        modelo.addAttribute("nro6", r.nextInt());
+    @RequestMapping("/")
+    public String inicio(){
         return "home";
     }
     
+    @RequestMapping("/pg2")
+    public String pg2(){
+        return "pagina2";
+    }
 }
